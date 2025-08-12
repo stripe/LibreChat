e535c9958a7c547323e086793d953cbfe59950c2
Matthew Mueller <mattmueller@stripe.com>
2025-08-12 14:48:17 -0500

    pass srcs from request to fetch calls

--- api/server/utils/index.js
+++ api/server/utils/index.js
@@ -4,8 +4,6 @@ const handleText = require('./handleText');
 const sendEmail = require('./sendEmail');
 const queue = require('./queue');
 const files = require('./files');
-const patchFetch = require('./patchFetch');
-
 
 module.exports = {
   ...handleText,
@@ -14,5 +12,4 @@ module.exports = {
   sendEmail,
   ...files,
   ...queue,
-  ...patchFetch,
 };
